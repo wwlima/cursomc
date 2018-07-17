09815386e37cc91791f96c037e27c1a9ad06ecc8
William Lima <wwlima@gmail.com>
2018-07-16 23:14:17 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/br/com/w2tec/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/w2tec/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package br.com.w2tec.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.w2tec.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest ok";
+	public List<Categoria> listar() {
+		
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(1,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+				
+		return lista;
 	}
 }
