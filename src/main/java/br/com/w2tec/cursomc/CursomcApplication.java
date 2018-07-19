e4be2c6f7e04dee16e28af944e664118e0845add
William Lima <wwlima@gmail.com>
2018-07-18 23:04:26 -0300

    Criando operação de instanciação - Inicializando tabela de categorias com alguns dados.

--- src/main/java/br/com/w2tec/cursomc/CursomcApplication.java
+++ src/main/java/br/com/w2tec/cursomc/CursomcApplication.java
@@ -1,12 +1,31 @@
 package br.com.w2tec.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.w2tec.cursomc.domain.Categoria;
+import br.com.w2tec.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		
 	}
 }
